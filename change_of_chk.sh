ee690f20721bb06d6ab86409a28cd590c08e9391
itsmarbo <marlenne.bf@gmail.com>
2023-03-27 12:12:27 +0200

    Add files

--- change_of_chk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *.com; do                                   
-    sed -i '3d' "$file"
-    sed -i "3r ${file%.com}.chk"                             
-done
--- fchk_converter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in *.chk; do
-    /shared/gaussian/g16.b01/SSE4/g16/formchk "$file"
-done
